5abb3ab3c6ccf710ce1b72d3d278ce51ce3fe0f9
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-27 00:46:15 +0900

    add query filtering

--- src/features/blog.ts
+++ src/features/blog.ts
@@ -3,8 +3,14 @@ import { Hono } from 'hono'
 const app = new Hono().basePath('/blog')
 
 app.get('/', (c) => {
-  const id = c.req.param('id')
-  return c.json([{ title: 'Blog 1' }, { title: 'Blog 2' }, { title: 'Blog 3' }])
+  const blogs = [{ title: 'Blog 1' }, { title: 'Blog 2' }, { title: 'Blog 3' }]
+
+  const { q } = c.req.query()
+  if (q) {
+    return c.json(blogs.filter((blog) => blog.title.includes(q)))
+  }
+
+  return c.json(blogs)
 })
 
 app.get('/:id', (c) => {
